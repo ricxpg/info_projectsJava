235c876e6177501087b338c2f6f69dc413e2fc95
Riccardo Puggioni <80036401+ricxpg@users.noreply.github.com>
2021-10-21 08:32:29 +0200

    incomplete

---
+++ es_uni/src/es_uni/Es_uni.java
@@ -0,0 +1,20 @@
+package es_uni;
+
+import java.util.Scanner;
+
+public class Es_uni {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int cycle;
+        cycle = sc.nextInt();
+        System.out.println("1. modify employee");
+        System.out.println("2. modify name of the UNI");
+        System.out.println("3. exit");
+        switch(cycle){
+            case 1:
+                
+        }
+    }
+    
+    
+}
